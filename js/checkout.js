72a19fd9f797cca063f66e7a9b097ed2ad0bca1b
Miquel de Riba <miqriba@gmail.com>
2021-10-24 00:19:41 +0200

    Aquest commit conté l'Sprint 3 en el Nivell 2 completat. #

--- js/checkout.js
+++ js/checkout.js
@@ -1,14 +1,66 @@
+// Exercici 8
+
+// Estils d'error - Els he fet amb CSS perquè la propia documentació de bootstrap diu que is-invalid no esta disponible encara
+
+// FALTA
+// -Ajustar el missatge d'error a cada error d'input: camp obligatori, no numeros etc
+
 // Get the input fields
-var password = document.querySelector(".password");
-var phone = document.querySelector('.phone');
-var name = document.querySelector('.name');
+const nom = document.querySelector(".name");
+const email = document.querySelector(".email");
+const adress = document.querySelector(".adress");
+const lastName = document.querySelector(".lastName");
+const password = document.querySelector(".password");
+const phone = document.querySelector(".phone");
+
+const form = document.querySelector(".form");
+const inputs = document.querySelectorAll(".form input");
 
 // Get the error elements
+var errorName = document.getElementById("errorName");
+var errorEmail = document.getElementById("errorEmail");
+var errorAdress = document.getElementById("errorAdress");
+var errorLastName = document.getElementById("errorLastName");
 var errorPassword = document.getElementById("errorPassword");
-var errorName = document.getElementById('errorName');  
-var errorPhone = document.getElementById('errorPhone');  
+var errorPhone = document.getElementById("errorPhone");
 
-// Exercise 9
-function validate() {
-    // Validate fields entered by the user: name, phone, password, and email
+const expresions = {
+  name: /^[a-zA-ZÀ-ÿ]{3,}$/,
+  email: /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-.]+\.[a-zA-Z0-9-.]/,
+  adress: /\w+/, // TO-DO
+  lastName: /^[a-zA-ZÀ-ÿ\s]{3,}$/,
+  password: /^[a-zA-Z0-9]{3,12}$/,
+  phone: /^\d{7,14}$/,
+};
+
+form.addEventListener("submit", validate);
+inputs.forEach((input) => {
+  input.addEventListener("keyup", validate); // opcional, que el missatge surti cada cop que aixequem una tecla molesta una mica
+  input.addEventListener("blur", validate); // al clicar fora també validem
+});
+
+// L'he volgut fer dinàmica: no faig servir els id individuals de cada missatge d'error perquè m'obligava a escriure un switch molt llarg. Potser no és la manera standard de ferho però volia veure si ho aconseguia fer no hardcoded.
+function validate(e) {
+  e.preventDefault();
+  console.log(`Funció validate activada`);
+  for (let exp in expresions) {
+    // recorre l'objecte expresions i comprova si el nom de l'element que ha disparat l'event coincideix amb la clau exp de expresions
+    if (exp === e.target.name) {
+      //aquí fem la comprovació regEx adaptada a cada camp
+      if (expresions[exp].test(e.target.value)) {
+        console.log("Correct " + exp);
+        document.querySelector("." + exp + " p").classList.add("hidden");
+
+        document
+          .querySelector("." + exp + " input")
+          .classList.remove("border-danger");
+      } else {
+        console.log("Incorrect " + exp);
+        document.querySelector("." + exp + " p").classList.remove("hidden");
+        document
+          .querySelector("." + exp + " input")
+          .classList.add("border-danger");
+      }
+    }
+  }
 }
--- js/grocery.js
+++ js/grocery.js
@@ -1,123 +1,337 @@
-// Exercise 11
-// Move this variable to a json file and load the data in this js
+// Detalls de la refactorització:
+
+// addToCart --> applyPromotionsCart --> calculateSubtotals --> calculateTotal
+
+/* Ho he canviat: calcular els descomptes al final de tot no tenia cap sentit perquè significava que havia de calcular els subtotals i total una altra vegada tenint en compte els descomptes. Per tant applyPromotionsCart ha passat a segon lloc i així no crido les funcions calculateSubtotals i calculateTotal dues vegades per sistema.
+
+
+
+Al final del document es troben les versions de les funcions per als primers 6 exercicis abans de refactoritzar.*/
+
 var products = [
-    {
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery'
-    },
-    {
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
-    },
-    {
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery'
-    },
-    {
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
-var cartList = [];
+  {
+    name: "Cooking oil",
+    price: 10.5,
+    type: "grocery",
+  },
+  {
+    name: "Pasta",
+    price: 6.25,
+    type: "grocery",
+  },
+  {
+    name: "Instant cupcake mixture",
+    price: 5,
+    type: "grocery",
+  },
+  {
+    name: "All-in-one",
+    price: 260,
+    type: "beauty",
+  },
+  {
+    name: "Zero Make-up Kit",
+    price: 20.5,
+    type: "beauty",
+  },
+  {
+    name: "Lip Tints",
+    price: 12.75,
+    type: "beauty",
+  },
+  {
+    name: "Lawn Dress",
+    price: 15,
+    type: "clothes",
+  },
+  {
+    name: "Lawn-Chiffon Combo",
+    price: 19.99,
+    type: "clothes",
+  },
+  {
+    name: "Toddler Frock",
+    price: 9.99,
+    type: "clothes",
+  },
+];
+// let cartList = [];
 var cart = [];
 var subtotal = {
-    grocery: {
-        value: 0, 
-        discount: 0
-    },
-    beauty: {
-        value: 0, 
-        discount: 0
-    },
-    clothes: {
-        value: 0, 
-        discount: 0
-    },
+  grocery: {
+    value: 0,
+    discount: 0,
+  },
+  beauty: {
+    value: 0,
+    discount: 0,
+  },
+  clothes: {
+    value: 0,
+    discount: 0,
+  },
 };
-var total = 0;
+let total = 0;
 
-// Exercise 1
-function addToCartList(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
-}
+// --⬇️ REFACTORITZAT ⬇️--
 
-// Exercise 2
-function cleanCart() {
-
-}
-
-// Exercise 3
-function calculateSubtotals() {
-    // 1. Create a for loop on the "cartList" array 
-    // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
-}
-
-// Exercise 4
-function calculateTotal() {
-    // Calculate total price of the cart either using the "cartList" array
-}
-
-// Exercise 5
-function applyPromotionsSubtotals() {
-
-}
-
-// Exercise 6
-function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-}
-
-// Exercise 7
-function applyPromotionsCart() {
-    // Apply promotions to each item in the array "cart"
-}
-
-// Exercise 8
 function addToCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+  // 1. Loop for to the array products to get the item to add to cart
+  // 2. Add found product to the cartList array
+  // addToCartList(id);
+
+  let trobat = false;
+  for (const a of cart) {
+    // si ja hi es ++ quantity
+    if (products[id - 1].name === a.name) {
+      trobat = true;
+      a.quantity++;
+      // console.log(
+      //   `S'ha trobat l'element ${
+      //     products[id - 1].name
+      //   } en el cart, ara n'hi ha ${a.quantity}.`
+      // );
+      break;
+    }
+  }
+  if (!trobat) {
+    // si no hi es en cart l'afegim
+    // console.log(
+    //   `No s'ha trobat l'element ${products[id - 1].name} en el cart. L'afegeim.`
+    // );
+    cart.push(products[id - 1]);
+    cart[cart.length - 1].quantity = 1;
+    cart[cart.length - 1].subtotal = 0;
+    cart[cart.length - 1].subtotalWithDiscount = 0;
+  }
+  // -- Refactorització: he decidit que després d'afegir cada producte individual es calculen els descomptes, subtotal i total automàticament, ja que això és el que esperaria de qualsevol web.
+  applyPromotionsCart();
+  calculateSubtotals();
+  calculateTotal();
 }
 
-// Exercise 9
+function applyPromotionsCart() {
+  // Apply promotions to each item in the array "cart"
+  // - Si l'usuari compra 3 o més ampolles d'oli, el preu del producte descendeix a 10 euros.
+  // - En comprar-se 10 o més mescles per a fer pastís, el seu preu es rebaixa a 2/3.
+  for (const a of cart) {
+    switch (a.name) {
+      case "Cooking oil":
+        if (a.quantity >= 3) {
+          a.subtotalWithDiscount = a.quantity * 10;
+
+          // console.log(`S'ha aplicat el descompte per a ${a.name}`);
+        }
+        break;
+      case "Instant cupcake mixture":
+        if (a.quantity >= 10) {
+          a.subtotalWithDiscount = (a.quantity * a.value * 2) / 3;
+
+          // console.log(`S'ha aplicat el descompte per a ${a.name}`);
+        }
+        break;
+    }
+  }
+}
+
+function calculateSubtotals() {
+  // Primer resetejem tots els subtotals:
+  for (const b in subtotal) {
+    subtotal[b].value = 0;
+    subtotal[b].discount = 0;
+  }
+  // Aquesta vegada ja estava més comode amb els objectes i els for.. of i for.. in i no m'ha calgut el switch statement:
+  for (const a of cart) {
+    for (const b in subtotal) {
+      if (a.type == b) {
+        // si el producte té un descompte, hem de sumar NOMÉS el preu amb descompte i NO el preu sense descomptar: els sumem a subtotal.value i només fem servir value, no discount.
+        if (a.subtotalWithDiscount) {
+          subtotal[b].discount += a.subtotalWithDiscount;
+          subtotal[b].value += a.subtotalWithDiscount;
+          // console.log(
+          //   `En aquest cas hi havia un subtotal del producte ${a.name} amb descompte que s'ha afegit als subtotals del tipus ${a.type}`
+          // );
+        } else {
+          a.subtotal = a.price * a.quantity;
+          subtotal[b].value += a.subtotal;
+          // console.log(
+          //   `S'ha afegit el valor del producte ${a.name} del cart a l'array de subtotals sota el tipus ${b}.`
+          // );}
+        }
+      }
+    }
+  }
+  console.log(subtotal);
+  console.log(cart);
+}
+
+function calculateTotal() {
+  // Calculate total price of the cart either using the "cartList" array
+  total = 0;
+  for (let x in subtotal) {
+    // CUIDADO: discount i value no son excloients: que en un tipus (grocery) hi hagi discount no vol dir que no hi hagi d'haver value.
+    // Com que hem inclos els valors tenint en compte descomptes i tot simplement en subtotal.value, ara només cal sumarlos:
+    total += subtotal[x].value;
+  }
+  console.log(`Total: ${total} $`);
+}
+
+function cleanCart() {
+  // falta buidar les quantitats
+  cartList = [];
+  cart = [];
+  total = 0;
+  for (const x in subtotal) {
+    subtotal[x].value = 0;
+    subtotal[x].discount = 0;
+  }
+  console.log("S'ha buidat el carro: ");
+  // mostrarCartList();
+  mostrarCart();
+}
+// Exercici 9
 function removeFromCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+  for (const a of cart) {
+    a.subtotalWithDiscount = 0;
+    if (products[id - 1].name === a.name) {
+      if (a.quantity === 1) {
+        cart.splice(a);
+        console.log(`S'ha suprimit l'element ${a.name} del cart`);
+      } else {
+        a.quantity--;
+        console.log(
+          `S'ha reduit la quantitat de l'element ${a.name} del cart de ${
+            a.quantity + 1
+          } a ${a.quantity}`
+        );
+      }
+    }
+  }
+  applyPromotionsCart();
+  calculateSubtotals();
+  calculateTotal();
+  console.log(cart);
 }
 
-
-
-// Exercise 10
 function printCart() {
-    // Fill the shopping cart modal manipulating the shopping cart dom
+  // Fill the shopping cart modal manipulating the shopping cart dom
+  document.querySelector(".list").innerHTML = "";
+
+  for (const a of cart) {
+    if (a.quantity > 1) {
+      document.querySelector(
+        ".list"
+      ).innerHTML += `<li>${a.name} (${a.quantity}) ${a.subtotal} $</li>`;
+    } else {
+      document.querySelector(
+        ".list"
+      ).innerHTML += `<li>${a.name} ${a.subtotal} $</li>`;
+    }
+  }
+  document.querySelector(".list").innerHTML += `<h6>Total: ${total} $</h6>`;
 }
+// function mostrarCartList() {
+//   //mostra per consola lestat actual de la cartList
+//   console.log("cartList: ");
+//   console.log(cartList);
+//   // for (i = 0; i < cartList.length; i++) {
+//   //   console.log(cartList[i]);
+//   // }
+// }
+function mostrarCart() {
+  console.log("Cart: ");
+  console.log(cart);
+}
+
+// ---⬇️ ABANS DE REFACTORITZAR ⬇️----
+//        (Exercicis 1 - 6)
+
+// function addToCart(id) {
+// 1. Loop for to the array products to get the item to add to cart
+// 2. Add found product to the cartList array
+// addToCartList(id);}
+
+// function calculateSubtotals() {
+// 1. Create a for loop on the "cartList" array
+// 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
+// Primer resetejem tots els subtotals:
+// for (const x in subtotal) {
+//   subtotal[x].value = 0;
+// }
+// for (const element of cartList) {
+//   // ARREGLAR com sentrava en un objecte?
+//   // Podria ser més general? que miri els tipus que hi han en el subtotal, no hard coded els if elses
+//   switch (element.type) {
+//     case "grocery":
+//       subtotal.grocery.value += element.price;
+//       console.log(
+//         `S'ha afegit ${element.price} a grocery, total grocery és ${subtotal.grocery.value}`
+//       );
+//       break;
+//     case "beauty":
+//       subtotal.beauty.value += element.price;
+//       console.log(
+//         `S'ha afegit ${element.price} a beauty, total beauty és ${subtotal.beauty.value}`
+//       );
+//       break;
+//     case "clothes":
+//       subtotal.clothes.value += element.price;
+//       console.log(
+//         `S'ha afegit ${element.price} a clothes, total clothes és ${subtotal.clothes.value}`
+//       );
+//       break;
+//   }
+// }
+// console.log(subtotal);}
+
+// function addToCartList(id) {
+//   // 1. Loop for to the array products to get the item to add to cart
+//   //   no he fet loop perquè l'item el puc agafar directament mitjançant products[id - 1]
+//   console.log(products[id - 1]);
+//   // 2. Add found product to the cartList array
+//   // de moment estic afegint el producte sencer com a objecte
+//   cartList.push(products[id - 1]);
+//   mostrarCartList();
+// }
+
+// function generateCart() {
+// Using the "cartlist" array that contains all the items in the shopping cart,
+// generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+//recorrem cada element del cartList
+
+//   const product = {
+//     name: "",
+//     price: 0,
+//     type: "",
+//     quantity: 0,
+//     subtotal: 0,
+//     subtotalWithDiscount: 0,
+//   };
+//   cart[product] = [];
+
+//   for (const a of cartList) {
+//     let trobat = false;
+//     for (i = 0; i < cart.length; i++) {
+//       // si ja hi es ++ quantity
+//       if (a.name === cart[i].name) {
+//         trobat = true;
+//         cart[i].quantity++;
+//         console.log(
+//           `S'ha trobat l'element de cartList ${a} en el cart, ara n'hi ha ${cart[i].quantity}.`
+//         );
+//         break;
+//       }
+//     }
+//     if (!trobat) {
+//       // si no hi es en cart l'afegim
+//       console.log(
+//         `No s'ha trobat l'element ${a} de cartList en el cart. L'afegeim.`
+//       );
+//       a.quantity = 1;
+//       a.subtotal = 0;
+//       a.subtotalWithDiscount = 0;
+//       cart.push(a);
+//     }
+//     mostrarCart();
+//   }
+// }
